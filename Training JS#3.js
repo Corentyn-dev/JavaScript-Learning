d6cdb8fb7208f086b17e2c517589bfeab75390b4
Corentyn Hayer <115353585+Corentyn-dev@users.noreply.github.com>
2023-10-12 17:57:48 +0200

    Create Training JS#3.js
    Creating some functions that return strings.

---
+++ Training JS#3.js
@@ -0,0 +1,26 @@
+/*
+ * Programming some functions that will return the string "Dad" "Bee" or "banana" depending on the variables. 
+ */
+
+//define some variables to combine "Dad" "Bee" and "banana"
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+
+//function that returns "Dad"
+function Dad(){
+  return d1 + a2 + d2;
+}
+
+//function that returns "Bee"
+function Bee(){
+  return b1 + e2 + e2;
+}
+
+//function that returns "banana"
+function banana(){
+  return b2 + a2+ n2+ a2 + n2 + a2;
+}
+
+//function that returns "answer"
+function answer1(){
+  return "no";
+}
